a380cb60dbdc7fb6194ad62c3c27c3e4c1b1b685
Valeiy Gladun <gladun1124@gmail.com>
2023-10-26 13:05:33 +0000

    change notifyV2

--- my_python_project/notify.py
+++ my_python_project/notify.py
@@ -1,11 +1,12 @@
-import telegram
-from telegram.ext import Updater, CommandHandler
+import requests
 
-# Замените <YOUR_BOT_TOKEN> на токен вашего бота
+# Замените 'YOUR_BOT_TOKEN' на ваш токен бота
 bot_token = "6693782514:AAGheL0DF6SNZXQGsks_2I80vlLvz9StyEA"
-
-# Замените <YOUR_CHAT_ID> на ID вашего чата
+# Замените 'YOUR_CHAT_ID' на chat_id вашего чата
 chat_id = "-1001988273555"
 
-bot = telegram.Bot(token=bot_token)
-await bot.send_message(chat_id=chat_id, text="Тесты завершились неудачей.")
+def send_telegram_message(message):
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    params = {'chat_id': chat_id, 'text': message}
+    response = requests.get(url, params=params)
+    return response.json() 
