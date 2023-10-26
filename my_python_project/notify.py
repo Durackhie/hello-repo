8a2a69c82e6506b0740b6a71dfd53b72f98438b3
Valeiy Gladun <gladun1124@gmail.com>
2023-10-26 13:18:56 +0000

    notifychange22222

--- my_python_project/notify.py
+++ my_python_project/notify.py
@@ -1,11 +1,25 @@
-import telegram
-from telegram.ext import Updater, CommandHandler
+import os
+from telegram import Bot
 
-# Замените <YOUR_BOT_TOKEN> на токен вашего бота
+# Токен вашего бота, полученный от BotFather
 bot_token = "6693782514:AAGheL0DF6SNZXQGsks_2I80vlLvz9StyEA"
 
-# Замените <YOUR_CHAT_ID> на ID вашего чата
+# Айди чата, куда вы хотите отправить уведомления
 chat_id = "-1001988273555"
 
-bot = telegram.Bot(token=bot_token)
-bot.send_message(chat_id=chat_id, text="Тесты завершились неудачей.")
+# Инициализация бота
+bot = Bot(token=bot_token)
+
+async def send_telegram_message(text):
+    await bot.send_message(chat_id=chat_id, text=text)
+
+if __name__ == "__main__":
+    # Ваш код или запуск тестов
+
+    # Если произошла ошибка
+    error_occurred = True
+
+    if error_occurred:
+        error_message = "Произошла ошибка с кодом 1"
+        # Отправка уведомления о ошибке
+        send_telegram_message(error_message)
