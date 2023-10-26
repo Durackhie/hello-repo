f83f6c9ff56685dffaab19c8c4ad0a4abfaca7c1
Valeiy Gladun <gladun1124@gmail.com>
2023-10-26 11:58:59 +0000

    add notify for yml

---
+++ notify.py
@@ -0,0 +1,11 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+# Замените <YOUR_BOT_TOKEN> на токен вашего бота
+bot_token = "6693782514:AAGheL0DF6SNZXQGsks_2I80vlLvz9StyEA"
+
+# Замените <YOUR_CHAT_ID> на ID вашего чата
+chat_id = "730829255"
+
+bot = telegram.Bot(token=bot_token)
+bot.send_message(chat_id=chat_id, text="Тесты завершились неудачей.")
