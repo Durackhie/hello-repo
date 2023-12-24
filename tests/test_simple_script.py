ecfccb8906ddd6231bddce6736e4fc3df007c2fe
Durackhie <gladun1124@gmail.com>
2023-12-24 04:41:45 +0000

    213123

---
+++ tests/test_simple_script.py
@@ -0,0 +1,15 @@
+import unittest
+from simple_script import add, subtract
+
+class TestSimpleScript(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(3, 5), 8)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(-1, -1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
