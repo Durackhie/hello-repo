90e2499fd9c5ee3c96a7c234078d78f44388b6a2
Durackhie <gladun1124@gmail.com>
2023-12-27 08:50:19 +0000

    HT

---
+++ src/mysql_class_test.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import MagicMock
+from mysql_class import MySQL
+
+class TestMySQLMethods(unittest.TestCase):
+    def setUp(self):
+        self.mock_cursor = MagicMock()
+        self.mock_connection = MagicMock()
+        self.mock_connection.cursor.return_value = self.mock_cursor
+
+        self.mysql = MySQL(host='127.0.0.1', user='Durackhie', password='Valera1124', database='DurackhieBase')
+        self.mysql.connection = self.mock_connection
+
+    def test_execute_query_success(self):
+        query = "SELECT * FROM table_name"
+        self.mysql.execute_query(query)
+        self.mock_cursor.execute.assert_called_once_with(query)
+        self.mock_connection.commit.assert_called_once()
+
+    def test_execute_query_failure(self):
+        query = "SELECT * FROM table_name"
+        self.mock_cursor.execute.side_effect = Exception("Mocked exception")
+        
+        with self.assertRaises(Exception):
+            self.mysql.execute_query(query)
+
+    def tearDown(self):
+        del self.mysql
+
+if __name__ == '__main__':
+    unittest.main()
+
