b5b30ae94ceaf1aebeb5ff38eb0cad6a5a5bf6cf
Durackhie <111425118+Durackhie@users.noreply.github.com>
2023-12-27 11:48:04 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,20 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from main import MySQL
 
 class TestMySQLMethods(unittest.TestCase):
-    def setUp(self):
+    @patch('pymysql.connect')
+    def setUp(self, mock_connect):
+        # Мокируем метод cursor объекта connect
+        mock_cursor = MagicMock()
+        mock_connect.return_value.cursor.return_value = mock_cursor
         self.mysql = MySQL(host='mock_host', user='mock_user', password='mock_pass', database='mock_db')
 
-    @patch.object(MySQL, 'execute_query')
-    def test_execute_query(self, mock_execute_query):
-        # Мокируем объект cursor внутри метода execute_query
-        mock_cursor = self.mysql.connection.cursor.return_value
-        self.mysql.execute_query("SELECT * FROM mock_table")
-        mock_cursor.execute.assert_called_once_with("SELECT * FROM mock_table")
+    def test_execute_query(self):
+        # Проверяем выполнение запроса
+        query = "SELECT * FROM mock_table"
+        self.mysql.execute_query(query)
+        self.mysql.cursor.execute.assert_called_once_with(query)
 
     def tearDown(self):
         del self.mysql
