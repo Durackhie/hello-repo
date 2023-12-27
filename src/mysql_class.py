348fe4667c41655c3526ebba6ccd02fe7efe299e
Durackhie <111425118+Durackhie@users.noreply.github.com>
2023-12-27 10:55:38 +0200

    Update mysql_class.py

--- src/mysql_class.py
+++ src/mysql_class.py
@@ -4,7 +4,7 @@ import pymysql
 class MySQL:
     def __init__(self, host, user, password, database):
         logging.basicConfig(filename='mysql_class.log', level=logging.DEBUG)
-        logging.info('Initializing MySQL connection')
+        logging.info('Инициализация подключения к MySQL')
         try:
             self.connection = pymysql.connect(
                 host='127.0.0.1',
@@ -13,22 +13,22 @@ class MySQL:
                 database='DurackhieBase'
             )
             self.cursor = self.connection.cursor()
-            logging.info('MySQL connection established')
+            logging.info('Соединение с MySQL установлено')
         except Exception as e:
-            logging.error(f'Error initializing MySQL connection: {e}')
+            logging.error(f'Ошибка инициализации подключения к MySQL: {e}')
 
     def execute_query(self, query):
-        logging.info(f'Executing query: {query}')
+        logging.info(f'Выполнение запроса: {query}')
         try:
-            self.cursor.execute(query)
+            self.cursor.execute(query)  # Убедимся, что метод execute вызывается
             self.connection.commit()
-            logging.info('Query executed successfully')
+            logging.info('Запрос успешно выполнен')
         except Exception as e:
-            logging.error(f'Error executing query: {e}')
+            logging.error(f'Ошибка выполнения запроса: {e}')
 
     def __del__(self):
         try:
             self.connection.close()
-            logging.info('MySQL connection closed')
+            logging.info('Соединение с MySQL закрыто')
         except Exception as e:
-            logging.error(f'Error closing MySQL connection: {e}')
+            logging.error(f'Ошибка закрытия соединения с MySQL: {e}')
