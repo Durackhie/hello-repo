57691b0a82be6586c01083c5959c0d1f0e7f2f3a
Durackhie <111425118+Durackhie@users.noreply.github.com>
2023-12-27 11:44:13 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,20 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import patch
 from main import MySQL
 
 class TestMySQLMethods(unittest.TestCase):
     def setUp(self):
-        # Мокируем объект MySQL для тестирования без реального подключения к базе данных
         self.mysql = MySQL(host='mock_host', user='mock_user', password='mock_pass', database='mock_db')
 
-    def test_execute_query(self):
-        # Проверяем выполнение запроса
-        query = "SELECT * FROM mock_table"
-        self.mysql.cursor.execute = MagicMock(return_value=True)
-        self.mysql.execute_query(query)
-        self.mysql.cursor.execute.assert_called_once_with(query)
+    @patch.object(MySQL, 'execute_query')
+    def test_execute_query(self, mock_execute_query):
+        # Мокируем объект cursor внутри метода execute_query
+        mock_cursor = self.mysql.connection.cursor.return_value
+        self.mysql.execute_query("SELECT * FROM mock_table")
+        mock_cursor.execute.assert_called_once_with("SELECT * FROM mock_table")
 
-    # Добавьте другие тесты по необходимости
+    def tearDown(self):
+        del self.mysql
 
 if __name__ == '__main__':
     unittest.main()
-
